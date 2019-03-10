180380d7f7e548342f7e15715fd53de32f4930f4
M Selvakumar <selva.developer@gmail.com>
2019-03-10 17:40:31 +0530

    adds verbose flag to control output

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class CONFIG:
     #FORMAT_STRING = "%(levelname)-8s:%(name)s.%(funcName)s>> %(message)s"
     FORMAT_STRING = "%(levelname)-8s:%(name)-8s.%(funcName)-8s>> %(message)s"
     LOGLEVEL = logging.CRITICAL
+    VERBOSE = False
