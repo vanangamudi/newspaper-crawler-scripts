9bbef445164279354ea8a9653fe03099a6eb50b6
M Selvakumar <selva.developer@gmail.com>
2019-03-10 17:42:16 +0530

    store data in ../data/<lang>/<scriptname>

--- tamil/crawler-viduthalai2.py
+++ tamil/crawler-viduthalai2.py
@@ -26,7 +26,12 @@ name = '{}'.format(uid_)
 class ViduthalaiCrawler(Crawler):
 
     def __init__(self, root_url):
-        root_dir = os.path.basename(__file__).replace('crawler-', '').replace('.py', '')
+        root_dir= os.path.abspath(__file__)
+        root_dir = '/'.join(root_dir.split('/')[-2:])
+        root_dir = root_dir.replace('crawler-', '').replace('.py', '')
+
+        print('root directory for storing data is {}'.format(root_dir))
+        
         super().__init__(root_url = root_url,
                          root_dir= root_dir)
 
